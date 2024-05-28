babe361e27652e4a2d41b5742b54520dc92974a6
Dai Hang <167784753+daihangpku@users.noreply.github.com>
2024-05-28 16:29:14 +0800

    header1.22

--- header.hpp
+++ header.hpp
@@ -14,6 +14,10 @@ class Game{
     string game_info;
     string team1;
     string team2;
+    void Print() {
+        cout << game_name << endl << game_info << endl;
+        cout << team1<< endl << team2 << endl;
+    }
     Game(string _name,string _team1,string _team2,string _info=""):game_name(_name),game_info(_info),team1(_team1),team2(_team2){}
 };
 class Score{
@@ -35,11 +39,11 @@ public:
     string name;//球员姓名
     string num;//球员号码
     //关于得分
-    vector<Score> points[5];//个人得分记录0~3=1~4节 4=决胜期
-    int sum_score_of_quarter[5];//单节得分
+    vector<Score> points[10];//个人得分记录0~3=1~4节 4=决胜期
+    int sum_score_of_quarter[10];//单节得分
     int total_score;//总得分
     //关于个人犯规
-    vector<Foul> foul[5];//个人犯规0~3=1~4节 4=决胜期
+    vector<Foul> foul[10];//个人犯规0~3=1~4节 4=决胜期
     int total_foul;//总犯规
     int special_foul;
     Player(string _name,string _num):num(_num),name(_name){
@@ -48,6 +52,9 @@ public:
 
         total_foul=0;
         special_foul=0;
+    }
+    void Print() {
+        cout << name << " " << num << endl;
     }
     void get_score(int _pts,int _time,int _num_of_quarter){//球员得分
         points[_num_of_quarter-1].push_back(Score(_pts,_time));
@@ -72,7 +79,7 @@ public:
 class Headcoach{
     public:
     string name;
-    vector<Foul> foul[5];//个人犯规0~3=1~4节 4=决胜期
+    vector<Foul> foul[10];//个人犯规0~3=1~4节 4=决胜期
     int total_foul;//总犯规
     int special_foul_B;
     int special_foul_C;
@@ -102,7 +109,9 @@ class Headcoach{
         special_foul_B=0;
         special_foul_C=0;
     }
-
+    void Print() {
+        cout << name << endl;
+    }
 };
 class Team {
 public:
@@ -112,12 +121,12 @@ public:
 	Player* players[12];															//第一个球员指针为player[0]
 	short present_player[13] = {0};                                                 //初始化为0,1在场,0不在场,-1被罚下
 	Headcoach* head_coach;
-	int score_of_each_quarter[5];													//第一节得分为score_of_each_quarter[0]
-	int team_foul_cnt[5];                                                          //第一节犯规数位team_faul_cnt[0]
+	int score_of_each_quarter[10];													//第一节得分为score_of_each_quarter[0]
+	int team_foul_cnt[4];                                                          //第一节犯规数位team_faul_cnt[0]
     int total_team_score;
 	void get_foul(Player* _player,int _penalty,char _type,int _time,int _num_of_quarter){
-	team_foul_cnt[_num_of_quarter-1]++;
-        if(team_foul_cnt[_num_of_quarter-1]>=4){
+	team_foul_cnt[min(_num_of_quarter-1,3)]++;
+        if(team_foul_cnt[min(_num_of_quarter-1,3)]>=4){
             //
         }
 	    _player->get_foul(_penalty,_type,_time,_num_of_quarter);
@@ -128,12 +137,53 @@ public:
 		score_of_each_quarter[num_of_quarter-1] += pts;                               //第一节对应的num_of_quarter=0
         total_team_score+=pts;
 	}
-	pair<int,int> first_half_timeout[2];//make_pair(num_of_quarter,second)
+	pair<int,int> first_half_timeout[2];//make_pair(second,num_of_quarter)
     int first_half_timeout_used;
 	pair<int,int> second_half_timeout[3];
     int second_half_timeout_used;
-	
-	Team(){}
+    pair<int,int> ot_timeout[6];
+    int ot_timeout_used[6];
+	void get_timeout(int _time,int num_of_quarter){
+        if(num_of_quarter<=1){
+            if(first_half_timeout_used<2){
+                first_half_timeout[first_half_timeout_used]=make_pair(_time,num_of_quarter);
+                first_half_timeout_used++;
+            }
+            else{
+                //不能叫暂停了；
+            }
+        }
+        else if(num_of_quarter<=3){
+            if(num_of_quarter==3&&_time>=8*60&&second_half_timeout_used==0){
+                second_half_timeout[second_half_timeout_used]=make_pair(-1,-1);
+                second_half_timeout_used++;
+            }
+            if(second_half_timeout_used<3){
+                second_half_timeout[second_half_timeout_used]=make_pair(_time,num_of_quarter);
+                second_half_timeout_used++;
+            }
+            else{
+                //不能叫暂停了；
+            }
+        }
+        else{
+            if(ot_timeout_used[num_of_quarter-5]==0){
+                ot_timeout[num_of_quarter-5]=make_pair(_time,num_of_quarter);
+                ot_timeout_used[num_of_quarter-5]=1;
+            }
+        }
+        
+    }
+	Team(){
+        for (int i = 0; i < 5; i++) {
+			score_of_each_quarter[i] = 0;
+			team_foul_cnt[i] = 0;
+		}
+		first_half_timeout_used = 0;
+		second_half_timeout_used = 0;
+        total_team_score=0;
+        memset(ot_timeout_used,0,sizeof(ot_timeout_used));
+    }
 	Team(string _name, bool _is_first_team, Player** _players,int _player_number, Headcoach* _headcoach):num_of_player(_player_number),name(_name) {
 		
 		is_first_team = _is_first_team;
@@ -146,7 +196,15 @@ public:
 		first_half_timeout_used = 0;
 		second_half_timeout_used = 0;
         total_team_score=0;
+        memset(ot_timeout_used,0,sizeof(ot_timeout_used));
 	}
+    void Print() {
+        cout << name << " " << num_of_player << endl;
+        for (int i = 0; i < num_of_player; i++) {
+            players[i]->Print();
+        }
+        head_coach->Print();
+    }
 };
 
 
@@ -174,7 +232,9 @@ class Score_event:public Event{
         pts=_pts;
     }
     void Print(){
-        cout<<event_type<<" "<<
+        cout<<event_type<<" "<<event_time.first<<" "<<event_time.second<<" ";
+        cout<<players_team<<" "<<player_num_in_list<<" ";
+        cout<<pts<<endl;
     }
 };
 class Foul_event:public Event{
@@ -187,12 +247,21 @@ class Foul_event:public Event{
         players_team=_players_team;
         foul=_foul;
     }
+    void Print(){
+        cout<<event_type<<" "<<event_time.first<<" "<<event_time.second<<" ";
+        cout<<players_team<<" "<<player_num_in_list<<" ";
+        cout<<foul.penalty<<" "<<foul.type<<" "<<foul.time<<endl;
+    }
 };
 class Timeout_event:public Event{
     public:
     int team;
     Timeout_event(string _event_type,pair<int,int> _event_time,int _team):Event(_event_type,_event_time){
         team=_team;
+    }
+    void Print(){
+        cout<<event_type<<" "<<event_time.first<<" "<<event_time.second<<" ";
+        cout<<team<<endl;
     }
 };
 #endif
